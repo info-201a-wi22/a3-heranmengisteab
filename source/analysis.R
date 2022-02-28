0b517deca59269a1ed541e50a57d424557db59af
heranmengisteab <heranm@uw.edu>
2022-02-28 09:36:54 -0800

    fix problems

--- source/analysis.R
+++ source/analysis.R
@@ -1,5 +1,6 @@
 library("dplyr")
 library("tidyverse")
+library("ggplot2")
 
 
 filename <- "https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv"
@@ -18,10 +19,10 @@ highest_incarceration_state
 
 
 #Variable 2: adult vs juvenile incarceration rate
-fem_adult <- sum(incarceration_trends$female_adult_jail_pop, na.rm = TRUE)
-mal_adult <- sum(incarceration_trends$male_adult_jail_pop, na.rm = TRUE)
-fem_juv <- sum(incarceration_trends$female_juvenile_jail_pop, na.rm = TRUE)
-mal_juv <- sum(incarceration_trends$male_juvenile_jail_pop, na.rm = TRUE)
+fem_adult <- sum(incarceration_trends$female_adult_jail_pop, na.rm = T)
+mal_adult <- sum(incarceration_trends$male_adult_jail_pop, na.rm = T)
+fem_juv <- sum(incarceration_trends$female_juvenile_jail_pop, na.rm = T)
+mal_juv <- sum(incarceration_trends$male_juvenile_jail_pop, na.rm = T)
 age <- c(fem_adult, fem_juv, mal_adult, mal_juv)
 data <- data.frame(age = c("Adult female", "Adult male", "juvenile female", "juvenile male"),
                    pop = c(fem_adult, mal_adult, fem_juv, mal_juv))
@@ -56,21 +57,34 @@ state_low_jail_pop
 
 
 #Variable 5: total incarceration population in Florida  
-total_incarceration_FL <- incarceration_trends %>%
-  select(state) %>%
-  filter(state == "FL") %>%
-  summarise(total_pop == max(total_jail_pop, na.rm = TRUE)) %>%
-  filter(total_pop == max(total_pop)) %>%
-  pull(total_pop)
+total_incarceration_FL <- incarceration_trends %>% 
+  filter(state == "FL") %>% 
+  group_by(year) %>% 
+  select(year, aapi_jail_pop, black_jail_pop, native_jail_pop, white_jail_pop, total_jail_pop) %>% 
+  na.omit()
 
-total_incarceration_FL 
+total_incarceration_FL
+
+
 
 #trends over time: native jail population from 1970-2018
-native_jail_pop_time <- ggplot(data = incarceration_trends) +
-  geom_col(mapping = aes(x = year , y = native_jail_pop, fill = year)) +
-  labs(title = "Native Jail Population Over Time") +
-  scale_alpha_continuous(5, 10)
+na_incarceration<- incarceration_trends %>% group_by(year) %>% 
+  select(year, native_jail_pop) %>%
+  gather(key = race, value = population, -year, na.rm = TRUE) %>%
+  group_by(year, race) %>%
+  summarise(population = sum(population))
 
+
+na_incarceration$race <- na_incarceration$race %>% 
+  factor(levels = c("native_jail_pop"))
+
+
+native_jail_pop_time <- ggplot(CA_incarceration_file)+
+  geom_col(mapping = aes(x = year, y = population, fill = race), position = "dodge") +
+  scale_alpha_continuous(5, 10) +
+  labs(title = "Native Jail Population Over Time", 
+       x = "year",
+       y = "total incarcerations", fill = "incarcerations")
 native_jail_pop_time
 
 
@@ -78,8 +92,8 @@ native_jail_pop_time
 female_vs_male <- incarceration_trends %>%
   group_by(state) %>%
   filter(year == 2000) %>% 
-  summarize(female_adult_jail_pop = mean(female_adult_jail_pop, na.rm = TRUE),
-            male_adult_jail_pop = mean(male_adult_jail_pop, na.rm = TRUE))
+  summarize(female_adult_jail_pop = mean(female_adult_jail_pop, na.rm = T),
+            male_adult_jail_pop = mean(male_adult_jail_pop, na.rm = T))
 
 adult_fem_v_mal <- ggplot(female_vs_male, aes(x = female_adult_jail_pop,
                                                       y = male_adult_jail_pop)) +
@@ -97,7 +111,7 @@ aapi_map <- incarceration_trends %>%
   select(year, state, aapi_jail_pop) %>%
   group_by(state) %>%
   filter(year == 2010) %>% 
-  summarize(aapi_jail_pop = sum(aapi_jail_pop, na.rm = TRUE))
+  summarize(aapi_jail_pop = sum(aapi_jail_pop, na.rm = T))
 
 us_map <- plot_usmap(data = aapi_map, values = "aapi_jail_pop", color = "white") + 
   labs(title = "Number of Asian Americans and Pacific Islanders People Incarcerated by State") +
